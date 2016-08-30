82ddca746a877df0f0e53572ef83cb0315c4aa56
Marcus Hann <marcus.hann@dexdyne.com>
2016-08-30 15:32:41 +0100

    set toctree only

--- docs/conf.py
+++ docs/conf.py
@@ -263,7 +263,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'cloud-fuse.tex', u'cloud-fuse Documentation',
-     u'Marcus Hann', 'manual'),
+     u'Marcus Hann', 'manual', True),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
