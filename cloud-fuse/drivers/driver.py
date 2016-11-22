e46e3675951f8416baf83ee1f634570ccb761103
Marcus Hann <marcus.hann@dexdyne.com>
2016-11-22 15:38:18 +0000

    add necessary driver files

---
+++ cloud-fuse/drivers/driver.py
@@ -0,0 +1,15 @@
+class Driver:
+    def writeFile(self, fileName, fileContents):
+        return False
+
+    def deleteFile(self, fileName):
+        return False
+
+    def makeDirectory(self, directoryName):
+        return False
+
+    def deleteDirectory(self, directoryName):
+        return False
+
+    def init(self):
+        return False
---
+++ cloud-fuse/drivers/filesystem.py
@@ -0,0 +1,5 @@
+import drivers.driver
+
+class FileSystem(drivers.driver.Driver):
+    def deleteDirectory(self, directoryName):
+        return True
